b9d5658a1d7fd1dae45c88e71794a6c105aa9ad8
GABO3M <gabriel.espinoza@utec.edu.pe>
2019-08-29 22:05:09 -0500

    Ejercicio 6 terminado

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -4,3 +4,27 @@
  Y encuentra el mayor valor: 30
 
  */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num1, num2, num3;
+
+    cin >> num1 >> num2 >> num3;
+
+    if (num1 > num2 && num1 > num3){
+        cout << "Mayor valor es: " << num1;
+    }
+    else{
+        if (num2 > num1 && num2 > num3){
+            cout << "Mayor valor es: " << num2;
+        }
+        else{
+            cout << "Mayor valor es: " << num3;
+        }
+    }
+
+    return 0;
+}
